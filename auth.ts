a243625f148876b8dd1384db4542acc1982e6ab5
navidul hoque <hnavidul@gmail.com>
2024-11-24 20:18:33 +0600

    frontend

--- auth.ts
+++ auth.ts
@@ -1,24 +1,103 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 import GitHub from "next-auth/providers/github"
+import Credentials from "next-auth/providers/credentials"
+import axios from "axios"
+import { url } from "./url"
+
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+
     providers: [Google({
+        clientId: process.env.AUTH_GOOGLE_ID,
+        clientSecret: process.env.AUTH_GOOGLE_SECRET,
         authorization: {
             params: {
                 prompt: "consent",
                 access_type: "offline",
-                response_type: "code",
+                response_type: "code"
             },
         },
-    }), 
+    }),
     GitHub({
+        clientId: process.env.AUTH_GITHUB_ID,
+        clientSecret: process.env.AUTH_GITHUB_SECRET,
         authorization: {
             params: {
-              prompt: "consent",
-              access_type: "offline",
-              response_type: "code",
+                prompt: "consent",
+                access_type: "offline",
+                response_type: "code"
             },
-          },
+        },
+    }),
+    Credentials({
+
+        authorize: async (credentials) => {
+
+            try {
+                const response = await axios.post(url + "/auth/login", {
+                    email: credentials.email,
+                    password: credentials.password
+                })
+
+                let user
+
+                if (response.data.status) {
+
+                    user = response.data.user
+                    return user
+                }
+
+                else {
+                    return null
+                }
+
+            }
+
+            catch {
+                return null
+            }
+        }
     })],
+
+    callbacks: {
+        async signIn({ user, account }) {
+
+            if (account?.provider === "credentials") {
+
+                return !!user; // Allow sign-in if a user was returned by `authorize`
+            }
+
+            else {
+                // User signed in using OAuth provider
+                try {
+                    await axios.post(url + '/auth/registration', {
+                        email: user.email,
+                        username: user.name,
+                        photo: user.image,
+                        provider: account?.provider,
+                    })
+
+                    return true;
+                }
+
+                catch (error) {
+                    console.error(error);
+                    return false;
+                }
+            }
+        },
+
+        jwt({ token, account }) {
+            if (account) { // Account is available during sign-in
+                token.provider = account.provider
+            }
+            return token
+        },
+
+        session({ session, token }) {
+            session.user.provider = token.provider
+            return session
+        },
+    }
 })
