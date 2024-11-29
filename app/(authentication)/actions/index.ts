5414a6cbade03dbe551f2d28157df3696ae59ddd
navidul hoque <hnavidul@gmail.com>
2024-11-29 21:41:32 +0600

    frontend

---
+++ app/(authentication)/actions/index.ts
@@ -0,0 +1,18 @@
+'use server'
+
+import { signIn, signOut } from "@/auth";
+
+export async function login(formData: FormData) {
+  const action = formData.get('action') as string
+  await signIn(action, { redirectTo: "/" });
+}
+
+export async function logout() {
+  await signOut({ redirectTo: "/" })
+}
+
+
+// export async function credentialLogin({email, password}: {email: string, password: string}) {
+
+//   await signIn("credentials", {email, password, redirect: false})
+// }
