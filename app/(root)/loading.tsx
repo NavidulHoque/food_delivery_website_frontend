079230ba28c60100dddcd522eb4830f6a800f3af
navidul hoque <hnavidul@gmail.com>
2025-01-18 13:33:47 +0600

    frontend

---
+++ app/(root)/loading.tsx
@@ -0,0 +1,11 @@
+import Loader from "@/components/common/Loader";
+
+export default function Loading() {
+  return (
+    <section className="h-[82vh] flex-center py-8">
+
+        <Loader />
+      
+    </section>
+  )
+}
